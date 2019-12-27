08fbfcd847d4d7e21da26a1dc7ec81a0393447c7
JuantAldea <JuantAldea@users.noreply.github.com>
2019-12-27 20:53:58 +0100

    Clean main

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ fn main() {
         vec![
             Transaction::new(0, &String::from("0").repeat(64), 0, 1, 20),
             Transaction::new(0, &String::from("0").repeat(64), 0, 2, 20),
-            //Transaction::new(0, &String::from("0").repeat(64), 0, 3, 20),
         ],
     );
 
@@ -18,54 +17,44 @@ fn main() {
     println!("==========================================================================");
     let wallet1 = Wallet::new(&chain, 1);
     let wallet2 = Wallet::new(&chain, 2);
-    //let wallet3 = Wallet::new(&chain, 3);
-
     println!("Wallet1 {:}", wallet1);
     println!("Wallet2 {:}", wallet2);
     println!("==========================================================================");
 
-    //println!("{:}", wallet3);
-
     let block = wallet1.create_transaction(2, 15).unwrap();
-
     println!("New block: {}\n", block);
     chain.add_block(block);
-    println!("==========================================================================");
     println!("New chain:\n{}\n", chain);
-    println!("==========================================================================");
-    //println!("{:?}\n", chain.check_chain());
 
+    println!("==========================================================================");
     let wallet1 = Wallet::new(&chain, 1);
     let wallet2 = Wallet::new(&chain, 2);
-
     println!("Wallet1:\n{:}", wallet1);
     println!("Wallet2:\n{:}", wallet2);
+    println!("==========================================================================");
 
     let block = wallet1.create_transaction(2, 5).unwrap();
-
     println!("New block: {}\n", block);
     chain.add_block(block);
 
     println!("==========================================================================");
     let wallet1 = Wallet::new(&chain, 1);
     let wallet2 = Wallet::new(&chain, 2);
-
     println!("Wallet1:\n{:}", wallet1);
     println!("Wallet2:\n{:}", wallet2);
     println!("==========================================================================");
 
     let block = wallet2.create_transaction(1, 23).unwrap();
-
     println!("New block: {}\n", block);
     chain.add_block(block);
     println!("{}", chain);
+
     println!("==========================================================================");
     let wallet1 = Wallet::new(&chain, 1);
     let wallet2 = Wallet::new(&chain, 2);
-
     println!("Wallet1:\n{:}", wallet1);
     println!("Wallet2:\n{:}", wallet2);
-
     println!("==========================================================================");
+
     println!("{:?}\n", chain.check_chain());
 }
